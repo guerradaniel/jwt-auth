8a2fed15223f360ba11699b7851007604c5d1c80
Daniel Guerra <danielguerra.g12@gmail.com>
2021-06-02 16:04:41 -0300

    Criado os components, services e routes para autenticar.

--- jwt-auth/src/app/app-routing.module.ts
+++ jwt-auth/src/app/app-routing.module.ts
@@ -2,12 +2,33 @@ import { TaskFormComponent } from './tasks/task-form/task-form.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './layout/home/home.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
+import { LoginComponent } from './account/login/login.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
 
 
 const routes: Routes = [
-  { path: '', component: TaskListComponent },
-  { path: 'new', component: TaskFormComponent },
-  { path: 'edit/:id', component: TaskFormComponent }
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: TaskListComponent },
+      { path: 'new', component: TaskFormComponent },
+      { path: 'edit/:id', component: TaskFormComponent }
+    ]
+  },
+
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent},
+      { path: 'create-account', component: CreateAccountComponent}
+    ]
+  }
+
 ];
 
 @NgModule({
@@ -15,3 +36,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+/**
+ * 
+ */
